98922970f06da98af64276a6234ee2715891f0d0
HelenYO <38363712+HelenYO@users.noreply.github.com>
2018-12-15 16:59:19 +0300

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -12,6 +12,12 @@ using namespace std;
 int n;
 vector<pair<int, int>> array;
 
+struct comp {
+    bool operator() (const int &a, const int &b) {
+        return array[a].second < array[b].second;
+    }
+};
+
 int main() {
 
     freopen("schedule.in", "r", stdin);
@@ -26,12 +32,17 @@ int main() {
     }
     sort(array.begin(), array.end(), [](pair<int, int> a, pair<int, int> b) { return (a.first < b.first); });
 
-    priority_queue<int>deadlines;
+
+    priority_queue<int, vector<int>, comp>deadlines;
 
     long ans = 0;
     int curTime = 1;
     for (int i = 0; i < n; ++i) {
 
+        if (array[i].first == 0) {
+            ans += array[i].second;
+            continue;
+        }
         deadlines.push(array[i].second);
         if (array[i].first >= curTime) {
             curTime++;
